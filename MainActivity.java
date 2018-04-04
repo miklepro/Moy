e439bd34cbbb551a3671668dee511613c5dd2d4a
Mikle <prokopchuk_mihail@mail.ru>
2018-04-04 13:36:20 +0700

    xd
    vv

---
+++ MainActivity.java
@@ -0,0 +1,35 @@
+package ru.divlt.mytehnikum;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private SlidingUpPanelLayout slidingUpPanelLayout;
+
+    private ImageButton imageButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+
+
+        ImageButton imageButton = (ImageButton) findViewById (R.id.imageButton);
+
+        slidingUpPanelLayout = (SlidingUpPanelLayout)findViewById (R.id.sliding_layout);
+
+        imageButton.setOnClickListener (new OnClickListener());
+
+    }
+
+    private class OnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
