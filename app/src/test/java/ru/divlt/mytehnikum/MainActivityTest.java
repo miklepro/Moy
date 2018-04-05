3f8b4df1d48a2afc3d7d3774266caf9f5dea6688
Mikle <prokopchuk_mihail@mail.ru>
2018-04-05 12:12:29 +0700

    This my project

---
+++ app/src/test/java/ru/divlt/mytehnikum/MainActivityTest.java
@@ -0,0 +1,15 @@
+package ru.divlt.mytehnikum;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Михаил on 19.03.2018.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+    }
+
+}
